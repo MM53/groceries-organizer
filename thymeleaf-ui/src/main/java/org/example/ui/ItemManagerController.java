786f84169f08bf871e7aae52a54e16c34268fc3c
MM53 <28218664+mm53@users.noreply.github.com>
2021-04-09 13:50:41 +0200

    Perfrom item-actions in backend

--- thymeleaf-ui/src/main/java/org/example/ui/ItemManagerController.java
+++ thymeleaf-ui/src/main/java/org/example/ui/ItemManagerController.java
@@ -1,44 +1,61 @@
 package org.example.ui;
 
-import org.example.entities.aggregateRoots.Item;
+import org.example.application.ItemManager;
 import org.example.units.UnitType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ItemManagerController {
 
+    private ItemManager itemManager;
+
+    @Autowired
+    public ItemManagerController(ItemManager itemManager) {
+        this.itemManager = itemManager;
+    }
+
     @GetMapping("/items")
     public String listItems(Model model) {
         model.addAttribute("template", "itemsList");
-        model.addAttribute("items", Arrays.asList("Item1", "Item2", "Item3", "Item4"));
+        model.addAttribute("items", itemManager.listItems());
         return "layout/main";
     }
 
     @PostMapping("/items")
-    public String createItem(@RequestParam String itemName, @RequestParam String unitType, Model model) {
+    public RedirectView createItem(@RequestParam String name, @RequestParam String unitType) {
+        itemManager.createItem(name, UnitType.valueOf(unitType));
+        return new RedirectView("/items");
+    }
+
+    @GetMapping("/items/{name}")
+    public String listItems(@PathVariable("name") String name, Model model) {
         model.addAttribute("template", "manageItem");
-        model.addAttribute("item", new Item(itemName, UnitType.valueOf(unitType)));
+        model.addAttribute("item", itemManager.viewItem(name));
         return "layout/main";
     }
 
-    @GetMapping("/items/{id}")
-    public String listItems(@PathVariable("id") String id, Model model) {
-        model.addAttribute("template", "manageItem");
-        model.addAttribute("item", new Item(id, UnitType.WEIGHT));
-        return "layout/main";
+    @PostMapping("/items/delete")
+    public RedirectView deleteItem(@RequestParam String name) {
+        itemManager.deleteItem(name);
+        return new RedirectView("/items");
     }
 
-    @PostMapping("/items/{id}/names")
-    public String addItemName(@PathVariable("id") String id, @RequestParam String name, Model model) {
-        model.addAttribute("template", "manageItem");
-        final Item item = new Item(id, UnitType.WEIGHT);
-        item.addAlternativeName(name);
-        model.addAttribute("item", item);
-        return "layout/main";
+    @PostMapping("/items/{name}/names")
+    public RedirectView addItemName(@PathVariable("name") String name, @RequestParam String alternativeName) {
+        itemManager.addName(name, alternativeName);
+        return new RedirectView("/items/" + name);
+    }
+
+    @PostMapping("/items/{name}/removeName")
+    public RedirectView removeItemName(@PathVariable("name") String name, @RequestParam String alternativeName) {
+        itemManager.removeName(name, alternativeName);
+        return new RedirectView("/items/" + name);
     }
 }
