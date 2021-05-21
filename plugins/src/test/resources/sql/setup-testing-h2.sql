ef53bca0ac29f2b5b224ac79e48e2a1574f8216a
MM53 <28218664+MM53@users.noreply.github.com>
2021-05-21 11:52:39 +0200

    Update sql-files for tests

--- plugins/src/test/resources/sql/setup-testing-h2.sql
+++ plugins/src/test/resources/sql/setup-testing-h2.sql
@@ -1,4 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS public;
 SET SCHEMA public;
 RUNSCRIPT FROM 'classpath:sql/item.sql';
-RUNSCRIPT FROM 'classpath:sql/storedItem.sql'
+RUNSCRIPT FROM 'classpath:sql/storedItem.sql';
+RUNSCRIPT FROM 'classpath:sql/shoppingList.sql';
+RUNSCRIPT FROM 'classpath:sql/recipe.sql'
