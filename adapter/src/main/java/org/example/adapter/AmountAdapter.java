d553f1a164650364d6feb83947f5d0aafb803522
MM53 <28218664+mm53@users.noreply.github.com>
2021-05-29 17:40:14 +0200

    Remove whitespace

--- adapter/src/main/java/org/example/adapter/AmountAdapter.java
+++ adapter/src/main/java/org/example/adapter/AmountAdapter.java
@@ -10,7 +10,7 @@ public class AmountAdapter {
 
     public static Amount ExtractFromString(String amountString) {
         double value = Double.parseDouble(amountString.replaceAll("[^0-9]", ""));
-        Unit unit = switch (amountString.replaceAll("[0-9]", "")) {
+        Unit unit = switch (amountString.replaceAll("[0-9]", "").replaceAll(" ", "")) {
             case "g" -> Weight.GRAM;
             case "kg" -> Weight.KILOGRAM;
             case "l" -> Volume.LITER;
