9a383de636b3884011053d2e9bc9c5c7fe15dc5e
MM53 <28218664+mm53@users.noreply.github.com>
2021-05-29 17:40:26 +0200

    Fix collector

--- adapter/src/main/java/org/example/adapter/persistence/jooq/mapper/collectors/RecipeCollector.java
+++ adapter/src/main/java/org/example/adapter/persistence/jooq/mapper/collectors/RecipeCollector.java
@@ -1,13 +1,9 @@
 package org.example.adapter.persistence.jooq.mapper.collectors;
 
 import org.example.entities.Ingredient;
-import org.example.entities.ItemName;
-import org.example.entities.aggregateRoots.Item;
 import org.example.entities.aggregateRoots.Recipe;
 import org.example.entities.aggregateRoots.Tag;
 import org.jooq.Record;
-
-import java.util.Optional;
 
 import static org.example.adapter.persistence.jooq.generated.Tables.*;
 
@@ -20,10 +16,16 @@ public class RecipeCollector extends RecordCollector<Recipe> {
 
     @Override
     Recipe updateEntityFromRecord(Record record, Recipe recipe) {
-        Optional.ofNullable(record.into(INGREDIENT).into(Ingredient.class))
-                .ifPresent(recipe::addIngredient);
-        Optional.ofNullable(record.into(TAG).into(Tag.class))
-                .ifPresent(recipe::addTag);
+        try {
+            Ingredient ingredient = record.into(INGREDIENT).into(Ingredient.class);
+            recipe.addIngredient(ingredient);
+        } catch (Exception ignored) {
+        }
+        try {
+            Tag tag = record.into(TAG).into(Tag.class);
+            recipe.addTag(tag);
+        } catch (Exception ignored) {
+        }
         return recipe;
     }
 }
