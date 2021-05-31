28b26463a5b9c940da9adbc367f66fe1ccfd201d
MM53 <28218664+MM53@users.noreply.github.com>
2021-05-31 16:31:42 +0200

    Load Recipe correctly if multiple tags and ingredients are used

--- adapter/src/main/java/org/example/adapter/persistence/jooq/mapper/collectors/RecipeCollector.java
+++ adapter/src/main/java/org/example/adapter/persistence/jooq/mapper/collectors/RecipeCollector.java
@@ -5,6 +5,8 @@ import org.example.aggregates.Tag;
 import org.example.entities.Ingredient;
 import org.jooq.Record;
 
+import java.util.UUID;
+
 import static org.example.adapter.persistence.jooq.generated.Tables.*;
 
 public class RecipeCollector extends RecordCollector<Recipe> {
@@ -16,11 +18,14 @@ public class RecipeCollector extends RecordCollector<Recipe> {
 
     @Override
     Recipe updateEntityFromRecord(Record record, Recipe recipe) {
-        if (record.get(INGREDIENT.ID) != null){
+        if (record.get(INGREDIENT.ID) != null && recipe.findIngredient(UUID.fromString(record.get(INGREDIENT.ID))).isEmpty()) {
             recipe.addIngredient(record.into(INGREDIENT).into(Ingredient.class));
         }
         if (record.get(TAG.NAME) != null) {
-            recipe.addTag(record.into(TAG).into(Tag.class));
+            Tag tag = record.into(TAG).into(Tag.class);
+            if (!recipe.getTags().contains(tag)) {
+                recipe.addTag(tag);
+            }
         }
         return recipe;
     }
